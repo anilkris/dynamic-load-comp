7bf3fdbbbc2b7ac68d9d8927f12754c996de0fd6
anil <anilkrishna@gmail.com>
2020-04-04 15:23:03 +0530

    added keyboard

--- src/app/keyboard/keyboard.component.ts
+++ src/app/keyboard/keyboard.component.ts
@@ -8,15 +8,18 @@ import { AdComponent } from '../ad-component';
 })
 export class KeyboardComponent implements OnInit, AdComponent {
 
+  NUMBER_KEYS = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-', '+', '<-'];
+  QWERTY_KEYS = ['->|', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']','\\'];
+  ASDF_KEYS = ['CAPS', 'A', 'S', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', ';', '\'','Enter'];
+  ZXCV_KEYS = ['SHIFT', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '?', '<','>'];
+  SPACE_KEY = [];
   @Input() data: any;
   constructor() { }
   ngOnInit(): void {
   }
 
   onClick(event) {
-
     this.data.elementRef.nativeElement.value = 'akshay';
-
   }
 
 
